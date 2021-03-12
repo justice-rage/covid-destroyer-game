56ecf0cac47af4a04d3ea8fe350769de6845abc7
Justice <justice.douglas@hey.com>
2021-03-12 09:00:48 -0500

    Added ability to quite game with esc key

--- covid-destroyer/game.py
+++ covid-destroyer/game.py
@@ -17,7 +17,11 @@ class COVID:
         pygame.display.set_caption("COVID Destroyer")
 
     def _handle_input(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                quit()
 
     def _process_game_logic(self):
         pass
