3b15d98b09e5f21b44702b575f19e4514aaae92d
Justice <justice.douglas@hey.com>
2021-03-14 18:21:08 -0400

    Created temporary win and lose messages for ending game

--- covid-destroyer/game.py
+++ covid-destroyer/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from models import Asteroid, Spaceship
-from utils import get_random_position, load_sprite
+from utils import get_random_position, load_sprite, print_text
 
 
 class SpaceRocks:
@@ -12,6 +12,8 @@ class SpaceRocks:
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
 
         self.asteroids = []
         self.bullets = []
@@ -69,6 +71,7 @@ class SpaceRocks:
             for asteroid in self.asteroids:
                 if asteroid.collides_with(self.spaceship):
                     self.spaceship = None
+                    self.message = "You lost MOTHERFUCKER!"
                     break
 
         for bullet in self.bullets[:]:
@@ -83,11 +86,17 @@ class SpaceRocks:
             if not self.screen.get_rect().collidepoint(bullet.position):
                 self.bullets.remove(bullet)
 
+        if not self.asteroids and self.spaceship:
+            self.message = "You're a winner. :)"
+
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
 
         for game_object in self._get_game_objects():
             game_object.draw(self.screen)
+
+        if self.message:
+            print_text(self.screen, self.message, self.font)
 
         pygame.display.flip()
         self.clock.tick(60)
--- covid-destroyer/utils.py
+++ covid-destroyer/utils.py
@@ -1,5 +1,6 @@
 import random
 
+from pygame import Color
 from pygame.image import load
 from pygame.math import Vector2
 from pygame.mixer import Sound
@@ -37,3 +38,12 @@ def get_random_velocity(min_speed, max_speed):
 def load_sound(name):
     path = f"assets/sounds/{name}.wav"
     return Sound(path)
+
+
+def print_text(surface, text, font, color=Color("tomato")):
+    text_surface = font.render(text, True, color)
+
+    rect = text_surface.get_rect()
+    rect.center = Vector2(surface.get_size()) / 2
+
+    surface.blit(text_surface, rect)
